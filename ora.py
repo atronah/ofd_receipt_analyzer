03f027b68b0aa117c838a6fb8e80fb316ae58500
atronah <atronah.ds@gmail.com>
2020-11-09 00:46:27 +0300

    ft: adds option to search by seller's INN (Taxpayer Identification Number)

--- ora.py
+++ ora.py
@@ -13,8 +13,9 @@ def cli():
 @click.option('-s', '--seller', help='seller''s name')
 @click.option('-d', '--date', 'search_date_str', help='receipt date (selling date)')
 @click.option('-e', '--email', help='seller''s email address')
+@click.option('-i', '--inn', help='seller''s INN')
 @click.argument('source', type=click.File('rt', encoding='utf8'))
-def search(seller, search_date_str, email, source):
+def search(seller, search_date_str, email, inn, source):
     for receipt_data in json.load(source):
         ticket = receipt_data['ticket']
         document = ticket['document']
@@ -30,6 +31,9 @@ def search(seller, search_date_str, email, source):
                 pprint(receipt)
 
         if email and email.upper() in receipt.get('sellerAddress', '').upper():
+            pprint(receipt)
+
+        if inn and inn in receipt.get('userInn', ''):
             pprint(receipt)
 
 
