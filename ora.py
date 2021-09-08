481baf5e9ae7463242348aa22b2def99b08a09a3
atronah <atronah.ds@gmail.com>
2021-09-08 17:02:30 +0300

    ft(ora): replaces `seller` option by `user` and `retailer`

--- ora.py
+++ ora.py
@@ -10,20 +10,25 @@ def cli():
      pass
 
 @click.command()
-@click.option('-s', '--seller', help='seller''s name')
+@click.option('-u', '--user', help='user''s name')
+@click.option('-r', '--retailer', help='retailer''s name')
 @click.option('-d', '--date', 'search_date_str', help='receipt date (selling date)')
 @click.option('-e', '--email', help='seller''s email address')
 @click.option('-i', '--inn', help='seller''s INN')
 @click.argument('source', type=click.File('rt', encoding='utf8'))
-def search(seller, search_date_str, email, inn, source):
+def search(user, retailer, search_date_str, email, inn, source):
     for receipt_data in json.load(source):
         ticket = receipt_data['ticket']
         document = ticket['document']
         receipt = document.get('receipt', None)
 
-        if seller and seller.upper() in receipt.get('user', '').upper():
         if not receipt:
             continue
+
+        if user and user.upper() in receipt.get('user', '').upper():
+            pprint(receipt)
+
+        if retailer and retailer.upper() in receipt.get('retailPlace', '').upper():
             pprint(receipt)
 
         if search_date_str:
