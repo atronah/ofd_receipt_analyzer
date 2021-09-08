5a5abe2d794ad6174bd47ea969e080109cf3507a
atronah <atronah.ds@gmail.com>
2021-09-08 19:17:33 +0300

    ft(ora): adds `stat` command
    to display total number of `receipts`/`bso` and frequencies of using field in all of them

--- ora.py
+++ ora.py
@@ -46,7 +46,47 @@ def search(user, retailer, search_date_str, email, inn, source):
             pprint(receipt)
 
 
+@click.command()
+@click.argument('source', type=click.File('rt', encoding='utf8'))
+def stat(source):
+    def update_statistic(statistic, data, r, i):
+        for key, value in data.items():
+            if not value:
+                continue
+
+            if isinstance(value, list):
+                simple_type = True
+                for item in value:
+                    if isinstance(item, dict):
+                        if isinstance(statistic.setdefault(key, [dict()]), dict):
+                            statistic[key] = [statistic]
+                        simple_type = False
+                        update_statistic(statistic[key][0], item, r, i)
+                if simple_type and value:
+                    if isinstance(statistic, list):
+                        statistic[0][key] = statistic[0].get(key, 0) + 1
+                    else:
+                        statistic[key] = statistic.get(key, 0) + 1
+            elif isinstance(value, dict):
+                update_statistic(statistic.setdefault(key, dict()), value, r, i)
+            else:
+                if isinstance(statistic, list):
+                    statistic = statistic[0]
+
+                statistic[key] = statistic.get(key, 0) + 1
+
+    info = {}
+    total_items = 0
+    for receipt_data in json.load(source):
+        update_statistic(info, receipt_data, receipt_data, info)
+        total_items += 1
+
+    pprint(info)
+    print(f'total receipts: {total_items}')
+
+
 cli.add_command(search)
+cli.add_command(stat)
 
 if __name__ == '__main__':
     cli()
